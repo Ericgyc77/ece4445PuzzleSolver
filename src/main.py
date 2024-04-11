5d9e966021ccbaf5bcb766d29583e3605c01b190
Eric Chen <egchen2@illinois.edu>
2024-04-11 11:42:58 -0500

    COde working on robot with 7 colors, need to improve lighting.

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ import time
 from util import create_mask
 from util import find_and_box_objects
 
-cap = cv2.VideoCapture(0) # Webcam, needs to be changed from PC to laptop
+cap = cv2.VideoCapture(1) # Webcam, needs to be changed from PC to laptop
 # cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
 # cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
@@ -32,18 +32,19 @@ while True:
 
 # Test program to identify singular pixel color using only hue value
 
+# 'lime': [((34, 255, 175), (74, 255, 255))],
+# 'red_wrap': [((171, 255, 255), (179, 255, 255)), (0, 0, 255)],
+
 # Define hue and value ranges for detection, ignoring saturation
 color_ranges = {
-    'red': [([0, 100, 100], [5, 255, 255])],
-    'orange': [([6, 100, 100], [21, 255, 255])],
-    'yellow': [([22, 100, 100], [33, 255, 255])],
-    # 'lime': [((34, 255, 175), (74, 255, 255))],
-    'green': [((34, 255, 40), (74, 255, 174))],
-    'cyan': [([75, 100, 100], [101, 255, 255])],   # STRONGGG
-    'blue': [([102, 100, 100], [120, 255, 255])],
-    'violet': [([150, 100, 100], [169, 255, 255])], # more like light magenta
-    'magenta': [([170, 100, 100], [178, 255, 255])], # florescent magenta
-    'red_wrap': [((171, 255, 255), (179, 255, 255))],
+    'red': [([0, 100, 100], [5, 255, 255], (0, 0, 255))],
+    'orange': [([6, 100, 100], [21, 255, 255], (0, 165, 255))],
+    'yellow': [([22, 100, 100], [33, 255, 255], (0, 255, 255))],
+    'green': [([34, 100, 40], [74, 255, 174], (0, 128, 0))],
+    'cyan': [([75, 100, 100], [101, 255, 255], (255, 255, 0))],   # STRONGGG
+    'blue': [([102, 100, 100], [120, 255, 255], (255, 0, 0))],
+    'violet': [([150, 100, 100], [169, 255, 255], (255, 0, 255))], # more like light magenta
+    'magenta': [([170, 100, 100], [178, 255, 255], (255, 0, 255))], # florescent magenta
 }
 
 # Change one of the magenta's, preferabbly lighter shade of magenta with a color lower on the hue scale more towards violet
@@ -58,41 +59,50 @@ while True:
     
     # Define color ranges for detection
     # Example for yellow (like a banana)
-    yellow_lower = np.array([162, 100, 100])
-    yellow_upper = np.array([178, 255, 255])
-    yellow_mask = cv2.inRange(hsv_frame, yellow_lower, yellow_upper)
+    # yellow_lower = np.array([162, 100, 100])
+    # yellow_upper = np.array([178, 255, 255])
+    # yellow_mask = cv2.inRange(hsv_frame, yellow_lower, yellow_upper)
     
-    cv2.imshow('Mask', yellow_mask)
+    # cv2.imshow('Mask', yellow_mask)
     
-    largest_area = 0
-    largest_rect = None
+    for color_name, ranges in color_ranges.items():
+        mask = None
+        for lower, upper, bbox_color in ranges:
+            temp_mask = cv2.inRange(hsv_frame, np.array(lower), np.array(upper))
+            if mask is None:
+                mask = temp_mask
+            else:
+                mask = cv2.bitwise_or(mask, temp_mask)
     
-    # Find contours and draw bounding box for yellow objects
-    contours, _ = cv2.findContours(yellow_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    for contour in contours:
-        if cv2.contourArea(contour) > 100:  # Filter out small areas
-            x, y, w, h = cv2.boundingRect(contour)
-            area = w * h
-            if area > largest_area:
-                largest_area = area
-                largest_rect = (x, y, w, h)
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 255), 2)  # Draw rectangle in yellow
-            
-    if largest_rect is not None:
-        x, y, w, h = largest_rect
-        center_x = x + w // 2               # find center of largest rectangle, flooring division 
-        center_y = y + h // 2
+        largest_area = 0
+        largest_rect = None
         
-        # Draw a dot at the center of the largest bounding box
-        cv2.circle(frame, (center_x, center_y), 5, (0, 0, 255), -1)
+        # Find contours and draw bounding box for yellow objects
+        contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        for contour in contours:
+            if cv2.contourArea(contour) > 100:  # Filter out small areas
+                x, y, w, h = cv2.boundingRect(contour)
+                area = w * h
+                if area > largest_area:
+                    largest_area = area
+                    largest_rect = (x, y, w, h)
+                cv2.rectangle(frame, (x, y), (x + w, y + h), bbox_color, 2)  # Draw rectangle in yellow
+                
+        if largest_rect is not None:
+            x, y, w, h = largest_rect
+            center_x = x + w // 2               # find center of largest rectangle, flooring division 
+            center_y = y + h // 2
+        
+            # Draw a dot at the center of the largest bounding box
+            cv2.circle(frame, (center_x, center_y), 5, (0, 0, 255), -1)
 
-        # Calculate the distance from the absolute center of the frame
-        frame_center_x = width // 2
-        frame_center_y = height // 2
-        distance = ((center_x - frame_center_x) ** 2 + (center_y - frame_center_y) ** 2) ** 0.5
-        
-        # Optionally, you can print or display the distance on the frame
-        print("Distance from center:", distance)
+            # Calculate the distance from the absolute center of the frame
+            frame_center_x = width // 2
+            frame_center_y = height // 2
+            distance = ((center_x - frame_center_x) ** 2 + (center_y - frame_center_y) ** 2) ** 0.5
+            
+            # Optionally, you can print or display the distance on the frame
+            print("Distance from center:", distance)
             
     ################################################ END OF NEW CODE ######################################################
 
